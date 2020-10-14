542516bcb0211d82ab45aed73dd714c07d3a797d
Muhammet ŞAFAK <muhametsafak@outlook.com>
2020-10-14 18:00:45 +0300

    Update Route.php

--- PHPXI/APPLICATION/Config/Route.php
+++ PHPXI/APPLICATION/Config/Route.php
@@ -10,6 +10,6 @@ define("DEFAULT_CONTROLLER_METHOD", "get|post");
 
 define("DEFAULT_CONTROLLER_404", "Welcome@not_found");
 
-define("FORCE_CONTROLLER_NAME", "Home");
+define("FORCE_CONTROLLER_NAME", "Welcome");
 
 define("FORCE_CONTROLLER_FUNCTION", "index");
